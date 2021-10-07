6a4d6667c5f081e3a5d6536b8b219b5311b530e1
gusals005 <gusals005@gmail.com>
2021-10-07 23:51:36 +0900

    [feat]영상에서도 angle 계산하는 코드 넣기

--- python/camconverter.py
+++ python/camconverter.py
@@ -38,11 +38,17 @@ def convertKeypointsfromCam():
             continue
 
         shoulder = [results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_SHOULDER].x,
-                    results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_SHOULDER].y]
+                    results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_SHOULDER].y,
+                    results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_SHOULDER].z]
         elbow = [results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_ELBOW].x, 
-                 results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_ELBOW].y]
+                 results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_ELBOW].y,
+                 results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_ELBOW].z]
         wrist = [results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_WRIST].x, 
-                 results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_WRIST].y]         
+                 results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_WRIST].y,
+                 results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_WRIST].z]
+
+        print(results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_SHOULDER].z)
+        print()         
 
         angle = calculateAngle(shoulder, elbow, wrist)
 
@@ -89,20 +95,20 @@ def calculateAngle(a,b,c):
     # angle1 = np.degrees(radians1)
 
     v1 = a-b
-    v2 = c-b    
+    v2 = c-b
     v1_norm = v1 / np.linalg.norm(v1)
     v2_norm = v2 / np.linalg.norm(v2)
     dot_product = np.dot(v1_norm, v2_norm)
     radian1 = np.arccos(dot_product)
     angle1 = np.degrees(radian1)
 
-    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
-    angle = np.abs(radians*180.0/np.pi)
-    print(angle1, 'vs', 360-angle)  
-    if angle >180.0:
-        angle = 360-angle
+    # radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    # angle = np.abs(radians*180.0/np.pi)
+    # print(angle1, 'vs', 360-angle)  
+    # if angle >180.0:
+    #     angle = 360-angle
         
-    return angle
+    return angle1
 
 
 if __name__ == "__main__":
